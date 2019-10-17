d6d6b13823472ba333d7cb8d1fc5f9c74e8f7916
Daniel <daniel.bird@codurance.com>
2019-10-17 17:25:16 +0100

    Second Commit

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      This app uses react, redux, react router and many other helpful libraries.
+    </p>
+  </div>
+);
+
+export default AboutPage;
---
+++ src/components/common/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  const activeStyle = { color: '#F15B2A' };
+
+  return (
+    <nav>
+      <NavLink exact to="/" activeStyle={activeStyle}>
+        Home
+      </NavLink>
+      {' | '}
+      <NavLink to="/about" activeStyle={activeStyle}>
+        About
+      </NavLink>
+      {' | '}
+      <NavLink to="/courses" activeStyle={activeStyle}>
+        Courses
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Header;
